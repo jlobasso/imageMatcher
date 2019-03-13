126cb73662c789a635c6070f46ce04aa79ba7815
andy <abertuccio@gmail.com>
2019-03-13 10:53:44 -0300

    match

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import numpy as np
-import cv2 as cv
+import cv2
 import matplotlib.pyplot as plt
 
-# print('hoal')
+img1 = cv2.imread('opencv-feature-matching-template.jpg',0)
+img2 = cv2.imread('opencv-feature-matching-image.jpg',0)
 
-img1 = cv.imread('box.png',0)          # queryImage
-img2 = cv.imread('box_in_scene.png',0) # trainImage
-# Initiate ORB detector
-orb = cv.ORB_create()
-# find the keypoints and descriptors with ORB
+orb = cv2.ORB_create()
+
 kp1, des1 = orb.detectAndCompute(img1,None)
 kp2, des2 = orb.detectAndCompute(img2,None)
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+matches = bf.match(des1,des2)
+matches = sorted(matches, key = lambda x:x.distance)
+
+print(len(matches))
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:235],None, flags=2)
+plt.imshow(img3)
+plt.show()
