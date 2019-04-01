5a7ff359fc0dc80add91fe44985df8c002ce1500
jlobasso <jlobasso@gmail.com>
2019-04-01 14:09:59 -0300

    match list JSON - REPO

--- matchList.py
+++ matchList.py
@@ -2,70 +2,67 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
+import urllib.request
+from function.searchRepo import * 
 
-MIN_MATCH_COUNT = 100
+def url_to_image(url):
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
 
-img1 = cv2.imread('images/n0p2.jpeg',0) 
-bestMatches = []
+def match(images, minMatchCount, scale, sensibility, minPercentMatch):
+    images2 = getImages()
+    len2 = len(images2)
+    globalMatches = []
 
-for x in range(1, 28):
+    #Recorre imagenes de livesearch
+    for x in range(0, len(images)):
+        bestMatches = []
+        img1 = cv2.resize(url_to_image(images[x]['image']), (scale, scale))
 
-    img2 = cv2.imread('images/n'+str(x)+'.jpeg',0)
-    sift = cv2.xfeatures2d.SIFT_create()
+        # recorre las imagenes originales del repo local
+        for y in range(0, len2):
+            img2 = cv2.resize(cv2.imread(images2[y], 0), (scale, scale))
 
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
+            sift = cv2.xfeatures2d.SIFT_create()
 
-    FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks = 50)
+            kp1, des1 = sift.detectAndCompute(img1, None)
+            kp2, des2 = sift.detectAndCompute(img2, None)
 
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
+            FLANN_INDEX_KDTREE = 0
+            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+            search_params = dict(checks=50)
 
-    matches = flann.knnMatch(des1,des2,k=2)
+            flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-    good = []
-    for m,n in matches:
-        if m.distance < 0.7*n.distance:
-            good.append(m)
+            matches = flann.knnMatch(des1, des2, k=2)
 
-    if len(good)>MIN_MATCH_COUNT:
-        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+            good = []
+            for m, n in matches:
+                if m.distance < sensibility*n.distance:
+                    good.append(m)
 
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-        matchesMask = mask.ravel().tolist()
 
-        h,w = img1.shape
-        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-        dst = cv2.perspectiveTransform(pts,M)
+            if float(len(good)/minMatchCount*100) > float(minPercentMatch):
+                bestMatches.append(
+                    {
+                        'image_url': str(images[x]['image']), 
+                        'percentage': str(len(good)/minMatchCount*100),
+                        'image_repo': str(images2[y]), 
 
-        img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+                    })
 
-    else:
-        
-        matchesMask = None
+            def extract(json):
+                try:
+                    return float(json['percentage'])
+                except KeyError:
+                    return 0
 
-    bestMatches.append({'image': str(x), 'percentage': str(len(good)/MIN_MATCH_COUNT*100)})
+            bestMatches.sort(key=extract, reverse=True)
+            
+        if len(bestMatches) > 0:
+            globalMatches.append(bestMatches)
 
-def extract(json):
-    try:        
-        return float(json['percentage'])
-    except KeyError:
-        return 0
+    return globalMatches
 
-bestMatches.sort(key=extract, reverse=True)
-
-fig=plt.figure(figsize=(8, 8))
-columns = 7
-rows = 4
-for x in range(1, 28):
-    imgMatch = cv2.imread('images/n'+str(bestMatches[x-1]['image'])+'.jpeg',0) 
-    fig.add_subplot(rows, columns, x)
-    plt.axis("off")
-    plt.title(str(round(float(bestMatches[x-1]['percentage']), 2))+'%')
-    plt.text(50,10, 'n' + bestMatches[x-1]['image'] + '.jpeg')
-     
-    plt.imshow(imgMatch, 'gray')
-plt.show()
-cv2.destroyAllWindows()
--- serverBakend.py
+++ serverBakend.py
@@ -4,48 +4,30 @@ from flask_cors import CORS
 from json import dumps
 import json 
 # from flask.ext.jsonpify import jsonify
-from matchListScaled import *
+from matchList import *
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
+min_match_count = 80
+scale = 200
+sensibility = 0.6
+min_percent_match = 20
+
 class Process(Resource):
     def post(self):
-        # print("----------------------------------------")
-        # print(request.data)
-        
 
         # print("----------------------------------------")
         # print(request.data)
-        # array = bytearray(request.data)
-        # print(b"abcde".decode("utf-8") array[1])
+        # print("----------------------------------------")
+
         data = json.loads(request.data)
-        
-        # print(data[0])
-        result = match(data)
-        # s = '[{"i":"imap.gmail.com","p":"someP@ss"},{"i":"imap.aol.com","p":"anoterPass"}]'
-        # jdata = json.loads(s)
-        # print(data[0])
-        # data = [{"a":"1"}]
-        # print(data[0]["a"])
-        # print(type(data))
-
-
-        x = {
-            "name": "John",
-            "age": 30,
-            "city": "New York"
-        }
-        
+        result = match(data, min_match_count, scale, sensibility, min_percent_match)
         
         return result, 200, {'Content-Type':'application/json'}
 
-
-        # return self.post.__init__
-
-api.add_resource(Process, '/process') # Route_1
-
+api.add_resource(Process, '/process') 
 
 if __name__ == '__main__':
     app.run(port='5002', debug=True)
