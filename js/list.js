e436bec941c9128d17684b0070fee7e9bc7efefb
andy <abertuccio@gmail.com>
2019-04-03 20:19:12 -0300

    front

--- js/list.js
+++ js/list.js
@@ -1,34 +1,8 @@
 const submit = document.getElementById("submit")
 const statusInfo = document.getElementById("status");
 const mainContainer = document.getElementById("main-container")
-
-const container = document.createElement("div");
-container.classList.add("div-block-2");
-
-title = document.createElement("div");
-title.classList.add("title");
-
-article = document.createElement("div");
-article.classList.add("mlid");
-
-row = document.createElement("div");
-row.classList.add("w-row");
-
-text = document.createElement("div");
-text.classList.add("text-block-2")
-
-col1 = document.createElement("div");
-col1.classList.add("w-col");
-col1.classList.add("w-col-6");
-
-col2 = document.createElement("div");
-col2.classList.add("w-col");
-col2.classList.add("w-col-6");
-
-
-img =  document.createElement("img");
-img.src = "";
-
+const statusContainer = document.getElementById("status-container")
+var cantImagesToCompare = 0;
 {/* <div class="div-block-2">
         <div class="title">Joico Moisture Recovery Condicionador 300ml<br></div>
         <div class="mlid">ML ID: <strong class="bold-text">MLB1002251313</strong></div>
@@ -47,7 +21,7 @@ img.src = "";
         </div>
       </div> */}
 
-var getStatus = ()=>{
+var getStatus = () => {
 
     fetch('status.json?' + Math.random() + '=' + Math.random())
         .then((response) => response.json())
@@ -57,9 +31,6 @@ var getStatus = ()=>{
                                     de ${status.running.of} 
                                     comparando con ${status.comparing.current} 
                                     de ${status.comparing.of}`
-            // if (Math.floor((status.absoluteComputed / (status.running.of * status.comparing.of)) * 100) === 100) {
-            //     clearInterval();
-            // }
         }
 
 
@@ -68,9 +39,12 @@ var getStatus = ()=>{
 }
 
 
-submit.addEventListener("click", async function () {
+submit.addEventListener("click", async () => {
     // const textarea = document.getElementById("Json")
     // const json = textarea.value;
+
+    const statusContainer = document.getElementById("status-container")
+    statusContainer.style.display = "block"
 
     var statusInterval = setInterval(() => {
         getStatus()
@@ -81,17 +55,10 @@ submit.addEventListener("click", async function () {
             return response.json();
         })
 
-    // console.log(json)
     let id = 0;
-    let imagenes = [];
+    var imagenes = [];
     json.forEach(a => {
-
-
-
         if ('images' in a && a.images.length > 0) {
-
-
-
             a.images.forEach((img, i) => {
                 if ('url' in img) {
                     imagenes.push({
@@ -105,6 +72,11 @@ submit.addEventListener("click", async function () {
         }
     });
 
+    /** OJO QUE ESTOY RECORTANDO EL ARRAY!!!! */
+    // imagenes = imagenes.slice(0, 2);
+
+    cantImagesToCompare = imagenes.length;
+
     var xhr = new XMLHttpRequest();
     xhr.open("POST", 'http://localhost:5000/process', true);
 
@@ -116,30 +88,24 @@ submit.addEventListener("click", async function () {
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             clearInterval(statusInterval)
 
-            container.appendChild(title)
-            container.appendChild(article)
-            
-            var title1 = title;
-            title1.innerHTML = "aaaaaaaaaaaaaacccccccccccccccccaaaaaaaaaaaaaaa"
-            col1.appendChild(title1)
-            col1.appendChild(image)
-            row.appendChild(col1)
+            // console.log(this.response)
 
-            var title2 = title;
-            col2.appendChild(title2)
-            col2.appendChild(image)
-            row.appendChild(col2)
-            
-            container.appendChild(row)
-
-
-            mainContainer.appendChild(container);
-            
-            console.log(this.response)
+            drawResults(this.response)
         }
     }
-    
-    data = {'imagenes': imagenes, 'min_match_count': 80, 'scale': 200, 'sensibility': 0.6, 'min_percent_match': 10};
+
+    let minMatchCount = +document.getElementById("min-match-count").value;
+    let scale = +document.getElementById("scale").value;
+    let sensibility = +document.getElementById("sensibility").value / 100;
+    let minPercentMatch = +document.getElementById("min-percent-match").value;
+
+    data = {
+        'imagenes': imagenes,
+        'min_match_count': minMatchCount,
+        'scale': scale,
+        'sensibility': sensibility,
+        'min_percent_match': minPercentMatch
+    };
 
     console.log(data)
 
@@ -152,3 +118,112 @@ submit.addEventListener("click", async function () {
 
     // console.log(imagenes)
 })
+
+
+drawResults = (data) => {
+
+    console.log(data);
+
+    data = JSON.parse(data);
+
+    let totalQuantity = 0;
+
+    let images = [];
+
+    data.matches.forEach(e => {
+        e.forEach(img => {
+            images.push(img)
+        })
+    })
+
+    images = images.sort((a, b) => {
+
+        if (a.percentage < b.percentage) {
+            return 1;
+        }
+        if (a.percentage > b.percentage) {
+            return -1;
+        }
+
+        return 0;
+    })
+
+        images.forEach(imgMatch => {
+
+            totalQuantity++;
+
+            var container = document.createElement("div");
+            container.classList.add("div-block-2");
+
+            var title = document.createElement("div");
+            title.classList.add("title");
+
+            var article = document.createElement("div");
+            article.classList.add("mlid");
+
+            var row = document.createElement("div");
+            row.classList.add("w-row");
+
+            var text = document.createElement("div");
+            text.classList.add("text-block-2")
+
+            var img1 = document.createElement("img");
+
+            var img2 = document.createElement("img");
+
+            title.innerHTML = "Este es el titulo"
+            article.innerHTML = "Este es como otro titulo"
+
+
+            container.appendChild(title)
+            container.appendChild(article)
+
+            var col1 = document.createElement("div");
+            col1.classList.add("w-col");
+            col1.classList.add("w-col-6");
+
+            var col2 = document.createElement("div");
+            col2.classList.add("w-col");
+            col2.classList.add("w-col-6");
+
+            var title1 = document.createElement("div");
+            var title2 = document.createElement("div");
+
+            title1.innerHTML = "imagen original";
+
+            col1.appendChild(title1)
+            img1.src = imgMatch.image_url
+            col1.appendChild(img1)
+            row.appendChild(col1)
+
+            title2.innerHTML = "Imagen catálogo marca";
+
+            col2.appendChild(title2)
+            img2.src = imgMatch.image_repo
+            col2.appendChild(img2)
+            row.appendChild(col2)
+
+
+            var percentage = document.createElement("div");
+            percentage.innerHTML = imgMatch.percentage + "%";
+            percentage.classList.add("percentage");
+
+
+            row.appendChild(percentage);
+
+            container.appendChild(row)
+
+
+            mainContainer.appendChild(container);
+        })
+
+
+    var resumenResultado = document.getElementById("resumen-resultado");
+    resumenResultado.innerHTML = `2. ${totalQuantity} Resultados encontrados sobre 
+    un total de ${cantImagesToCompare} publicaciones analizadas`;
+    resumenResultado.style.display = "block";
+
+
+
+
+}
