da7b560d50a990d7cd5432252862d13f06a23bb0
andy <abertuccio@gmail.com>
2019-05-08 09:27:39 -0300

    limpiamos groups

--- backend/getGroups.py
+++ backend/getGroups.py
@@ -8,6 +8,5 @@ def getGroups():
     conn = MongoClient()
     db = conn.imageMatcher
 
-    # response = [json.dumps(doc, default=json_util.default) for doc in db.groupCategories.find({},{"_id":0})]
     response = [doc for doc in db.groupCategories.find({},{"_id":0})]
     return response
